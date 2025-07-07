18908cf73c3757a8e7a30c3dbb187c236ac1d92d
satanismit <161738842+satanismit@users.noreply.github.com>
2025-07-07 22:34:58 +0530

    Time: 13 ms (31.88%) | Memory: 60.3 MB (9.71%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        
+        Set<Integer> set=new HashSet<>();
+
+        for(int i:nums){
+            set.add(i);
+        }
+        for(int i=1; i<=10000000; i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+        return 1;
+    }
+}
