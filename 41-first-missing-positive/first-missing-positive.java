a5978ce99a3eedb4e490c9e622af75db21f15c83
satanismit <161738842+satanismit@users.noreply.github.com>
2025-07-08 20:40:36 +0530

    Time: 1 ms (100.00%) | Memory: 57.5 MB (40.51%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,16 +1,24 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        
-        Set<Integer> set=new HashSet<>();
-
-        for(int i:nums){
-            set.add(i);
-        }
-        for(int i=1; i<=10000000; i++){
-            if(!set.contains(i)){
-                return i;
+        int i = 0;
+        while( i < nums.length){
+            if(nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]){
+                swap(nums,i,nums[i] - 1);
+            }else{
+                i++;
             }
         }
-        return 1;
+        for(i = 0;i<nums.length;i++){
+            if(nums[i] != i + 1){
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+    
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
