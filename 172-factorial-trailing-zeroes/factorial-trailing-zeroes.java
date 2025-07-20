bdd6aa46d3049b81811bf88d7e84bab633c7681a
satanismit <161738842+satanismit@users.noreply.github.com>
2025-07-20 17:40:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (48.62%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        
+        int cnt=0;
+        int m=0;
+
+        for(int i=5; n/i>=1; i*=5){
+
+            cnt+=n/(i);    
+
+        }
+        return cnt;
+
+
+    }
+}
