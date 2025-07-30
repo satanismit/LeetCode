ed4c690bd7d7bd899bc4b0fd4f987e56cb3439e1
satanismit <161738842+satanismit@users.noreply.github.com>
2025-07-30 21:47:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (88.61%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,29 +1,22 @@
 class Solution {
-
-    int[] arr=new int[101];
-
-    public int solve(int[] nums, int i){
-
-        //base
-        if(i>=nums.length){
-            return 0;
-        }
-
-        if(arr[i]!=-1){
-            return arr[i];
-        }
-
-        int steal =nums[i]+solve(nums, i+2);
-        int skip =solve(nums, i+1);
-
-        return arr[i]=Math.max(steal, skip);
-    }
-
-
-
     public int rob(int[] nums) {
         
-        Arrays.fill(arr,-1);
-        return solve(nums, 0);
+        int[] arr=new int[101];
+        int n=nums.length;
+
+        if(n==1) return nums[0];
+        arr[0]=0;
+        arr[1]=nums[0];
+        int steal;
+        int skip;
+
+        for(int i=2; i<=n; i++){
+
+            steal=nums[i-1]+arr[i-2];
+            skip=arr[i-1];
+            
+            arr[i]=Math.max(steal, skip);
+        }
+        return arr[n];
     }
 }
