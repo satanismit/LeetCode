c1936108f9b3e32a83d78d76298dd34774acab7d
satanismit <161738842+satanismit@users.noreply.github.com>
2025-08-03 09:32:08 +0530

    Time: 35 ms (41.28%) | Memory: 51.9 MB (35.47%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,29 +1,37 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-       
-       int m=grid.length;
-       int n=grid[0].length;
+        
+
+        int n=grid.length;
+        // use HashMap for O(n*2) time complexity 
+        Map<String,Integer> rowmap=new HashMap<>();
+
+        for(int i=0; i<n; i++){
+
+            //for string element in a string 
+            StringBuilder str=new StringBuilder();
+            for(int j=0; j<n; j++){
+
+                str.append(grid[i][j]).append(",");
+
+            }
+            //add row into map with frequency 
+            rowmap.put(str.toString(), rowmap.getOrDefault(str.toString(),0)+1);
+        }
+
         int cnt=0;
-       for(int i=0; i<m; i++){
 
-        for(int j=0; j<n; j++){
+        for(int i=0; i<n;i++){
 
-            if(equals(grid, i, j, n)){
-                cnt++;
-            }
+            StringBuilder colstr=new StringBuilder();
+            for(int j=0; j<n; j++){
+
+                colstr.append(grid[j][i]).append(",");
         }
-       }
-       return cnt;
-    }
+        cnt+=rowmap.getOrDefault(colstr.toString(), 0);
 
-    public boolean equals(int[][] arr, int i, int j,int n){
-
-        for(int k=0; k<n; k++){
-
-            if(arr[i][k]!=arr[k][j]){
-                return false;
-            }
         }
-        return true;
+
+        return cnt;
     }
 }
