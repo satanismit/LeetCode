f0411ba5a1bef7cf4e3309cf5adfb865024835e8
satanismit <161738842+satanismit@users.noreply.github.com>
2025-08-10 18:20:47 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (13.35%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,46 +1,39 @@
 class Solution {
     public int trap(int[] height) {
+        
+        int n=height.length;
+        int left=0, right=n-1;
 
-      int n=height.length;
+        int leftmax=0,rightmax=0;
+        int water=0;
 
-      int[] leftmax=new int[n];
-      int[] rightmax=new int[n];
-      int[] watertrap=new int[n];
-      int totalwater =0 ;
+        while(left<right){
 
-      //intilize first value in leftmax
-      leftmax[0]=height[0];
+            // left < right
+            if(height[left]<height[right]){
 
-      //rightmax 
-      rightmax[n-1]=height[n-1];
+                if(leftmax>=height[left]){
+                    water+=leftmax-height[left];
 
+                }else{
+                    leftmax=height[left];
+                }
+                left++;
+            }
+            //right > left
+            else{
 
-      
+                if(rightmax>=height[right]){
+                    water+=rightmax-height[right];
 
-        //fill leftmax array 
-        for(int j=1; j<n;j++){
+                }else{
+                    rightmax=height[right];
+                }
+                right--;
+            }
 
-            leftmax[j]=Math.max(leftmax[j-1], height[j]);
         }
 
-        //fill rightmax array
-        for(int j=n-2; j>=0;j--){
-
-            rightmax[j]=Math.max(rightmax[j+1], height[j]);
-        }
-
-        for(int j=0; j<n; j++){
-
-            //store small one 
-          watertrap[j]=Math.min(leftmax[j],rightmax[j])- height[j];
-            totalwater += watertrap[j];
-        }
-
-        return totalwater;
-
-
-      
-
-
+        return water;
     }
 }
