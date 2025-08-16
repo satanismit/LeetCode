4ce8bdba3783a2d4539bb7daddce0012e39c4dde
satanismit <161738842+satanismit@users.noreply.github.com>
2025-08-16 13:25:57 +0530

    Time: 1 ms (97.65%) | Memory: 42.1 MB (77.15%) - LeetSync

--- 3636-check-balanced-string/check-balanced-string.java
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -4,12 +4,15 @@ class Solution {
         int n=num.length();
         int evensum=0,oddsum=0;
 
+
+        //even numbers 
         for(int i=0; i<n; i+=2){
 
             evensum+=num.charAt(i)-'0';
 
         }
 
+        //odd numbers 
         for(int i=1; i<n; i+=2){
 
             oddsum+=num.charAt(i)-'0';
@@ -17,7 +20,6 @@ class Solution {
         }
 
         return evensum==oddsum;
-
 
 
     }
