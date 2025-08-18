ce87d14e31255d5fed8300804a0be5d02f3dca2a
satanismit <161738842+satanismit@users.noreply.github.com>
2025-08-18 21:02:57 +0530

    Time: 6 ms (100.00%) | Memory: 44.9 MB (46.84%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,56 +8,18 @@
  * }
  */
 class Solution {
-
-    public boolean getancestor(TreeNode root, TreeNode X,List<TreeNode> list){
-        
-        if(root==null){
-            return false;
-        }
-        
-        if(root==X){
-            return true;
-        }
-        if(getancestor(root.left,X,list) || getancestor(root.right, X,list)){
-            list.add(root);
-            return true;
-        }
-
-        return false;
-
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
 
-            List<TreeNode> list1=new ArrayList<>();
-             List<TreeNode> list2=new ArrayList<>();
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-            list1.add(p);
-             list2.add(q);
+        if (left != null && right != null) {
+            return root;
+        }
 
-             getancestor(root,p,list1);
-             getancestor(root,q,list2);
-
-             int n1=list1.size(),n2=list2.size();
-
-             Set<TreeNode> set=new LinkedHashSet<>();
-
-             for(int i=0; i<n1; i++){
-                set.add(list1.get(i));
-             }
-
-             boolean flag=false;
-
-             for(int i=0; i<n2; i++){
-
-                if(set.contains(list2.get(i))){
-                    
-                    return list2.get(i);
-                   
-                }
-             }
-
-            
-
-        return root;
-    }
+        return left == null ? right : left;
+    }    
 }
