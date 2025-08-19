cdd43e1225bb6bd7fd1d7034510941b54c8f2256
satanismit <161738842+satanismit@users.noreply.github.com>
2025-08-19 20:41:35 +0530

    Time: 1 ms (45.35%) | Memory: 51.3 MB (59.75%) - LeetSync

---
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int sum =0;
+
+    public void helper(TreeNode root,int low,int high){
+        //base 
+        if(root==null){
+            return;
+        }
+
+        //check range 
+         if(root.val>=low && root.val<=high){
+            sum+=root.val;
+        }
+
+
+        helper(root.left,low,high);
+        helper(root.right,low,high);
+    }
+    public int rangeSumBST(TreeNode root, int low, int high) {
+
+        helper(root,low,high);
+        return sum;
+       
+
+        
+
+    }
+}
