06da16dc9d91b55b27cf5eeae40302f00c8a3920
satanismit <161738842+satanismit@users.noreply.github.com>
2025-09-02 21:46:48 +0530

    Time: 1 ms (99.51%) | Memory: 57.1 MB (56.21%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int n=nums.length;
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++){
+
+           if(sum<0){
+             sum=0;
+           }
+
+           sum+=nums[i];
+           max=Math.max(sum,max);
+        }
+
+        return max;
+    }
+}
