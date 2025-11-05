34584f1fc04133fc3d9244d4be309936e013c1fa
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2025-11-05 12:11:15 +0530

    Time: 1 ms (33.85%) | Memory: 42.8 MB (5.51%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,17 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         
-        boolean flag = false;
-        double t =n;
-
-        while(t>0){
-
-            if(t==1){
-                flag=true;
-            }
-            t/=2;
-        }
-
-        return flag;
+        return n>0 && (((n-1)&n)==0);
     }
 }
