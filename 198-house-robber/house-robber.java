cbf79fb76e55d666c9b3c69b7a1f772a1b85aadf
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2025-12-12 17:08:22 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (6.97%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,30 +1,52 @@
 class Solution {
 
     // recursion + Memoization  
+    // public int solve(int[] nums,int i,int[] dp){
+
+    //     int n=nums.length;
+        
+    //     //base case 
+    //     if(i>=n){
+    //         return 0;
+    //     }
+
+    //     if(dp[i]!=-1){
+    //         return dp[i];
+    //     }
+
+    //     int skip = solve(nums,i+1,dp);
+    //     int steal = nums[i] + solve(nums,i+2,dp);
+
+    //     return dp[i]= Math.max(steal, skip);
+    // }
+
+
+    //tabulation method :
+
     public int solve(int[] nums,int i,int[] dp){
 
         int n=nums.length;
-        
-        //base case 
-        if(i>=n){
-            return 0;
+
+        if(n==1) return nums[0];
+        //starts from the base case 
+        dp[0]=nums[0];
+        dp[1]= Math.max(nums[0], nums[1]);
+
+        //iterate 
+        for(int j=2; j<n; j++){
+
+            int steal = nums[j] + dp[j-2];
+            int  skip = dp[j-1];
+
+            dp[j]= Math.max(steal, skip);
         }
 
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-
-        int skip = solve(nums,i+1,dp);
-        int steal = nums[i] + solve(nums,i+2,dp);
-
-        return dp[i]= Math.max(steal, skip);
+        return dp[n-1];
     }
     public int rob(int[] nums) {
         
         int n=nums.length;
         int[] dp = new int[n+1]; 
-
-        Arrays.fill(dp,-1);
         
         return solve(nums,0,dp);
     }
