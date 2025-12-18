cafa603d6b820950dfb512097b38e3e052e6ed36
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2025-12-18 21:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (31.14%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,26 +1,32 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
 
-        int[][] dp = new int[m][n];
+    public int solve(int i,int j,int[][] memo){
 
-        // base cases
-        for (int i=0; i<m; i++) {
-            dp[i][0] = 1;
-        }
+        //base case 
+        if(i==0 && j==0)  return 1;
 
-        for (int j=0; j<n; j++) {
-            dp[0][j] = 1;
-        }
+        if(i<0 || j<0)  return 0;
 
-        // fill dp table forward
-        for (int i=1; i<m; i++) {
+        if(memo[i][j]!=-1)   return memo[i][j];
 
-            for (int j=1; j<n; j++) {
 
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
+        int up = solve(i-1,j,memo);
+        int left = solve(i,j-1,memo);
 
-        return dp[m-1][n-1];
+        return memo[i][j]= up+left;
+
     }
+
+    public int uniquePaths(int m, int n) {
+        
+        int[][] memo =new int[m][n];
+
+        for(int[] rows:memo){
+            Arrays.fill(rows,-1);
+        }
+
+
+        return solve(m-1,n-1,memo);
+    }
+
 }
