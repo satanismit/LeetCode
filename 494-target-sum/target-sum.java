16e2c4a9d6712a004908cf84c8bd925cd9d06665
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2025-12-28 11:33:52 +0530

    Time: 111 ms (35.64%) | Memory: 47.6 MB (5.16%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,5 +1,7 @@
 class Solution {
 
+    Map<String , Integer> memo;
+
     //memoization method
     public int solve(int[] arr, int target, int i){
 
@@ -9,12 +11,18 @@ class Solution {
             return target==0 ? 1: 0;
         }
 
+        String key = i +"," +target;
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
+
         //add + 
         int add = solve(arr, target-arr[i], i+1);
 
         //minus -
         int sub = solve(arr, target+arr[i], i+1);
 
+        memo.put(key, add + sub);
         return add + sub;
 
     }
@@ -22,6 +30,8 @@ class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         
         int n=nums.length;
+
+        memo = new HashMap<>();
 
         return solve(nums, target, 0);
 
