53ee212fba9be7ecd1056829e238db24f80cc95a
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2025-12-28 11:43:33 +0530

    Time: 5 ms (80.76%) | Memory: 45.3 MB (36.02%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,39 +1,42 @@
 class Solution {
 
-    Map<String , Integer> memo;
-
-    //memoization method
-    public int solve(int[] arr, int target, int i){
-
-        //base case 
-        if(i==arr.length){
-            
-            return target==0 ? 1: 0;
-        }
-
-        String key = i +"," +target;
-        if(memo.containsKey(key)){
-            return memo.get(key);
-        }
-
-        //add + 
-        int add = solve(arr, target-arr[i], i+1);
-
-        //minus -
-        int sub = solve(arr, target+arr[i], i+1);
-
-        memo.put(key, add + sub);
-        return add + sub;
-
-    }
-
     public int findTargetSumWays(int[] nums, int target) {
         
-        int n=nums.length;
+        int totalSum = 0;
 
-        memo = new HashMap<>();
+        for (int n : nums) totalSum += n;
 
-        return solve(nums, target, 0);
+        // Base cases: target must be reachable and (totalSum + target) must be even
+        if (Math.abs(target) > totalSum || (totalSum + target) % 2 != 0) {
+            return 0;
+        }
 
+        int s1 = (totalSum + target) / 2;
+        int n = nums.length;
+
+        
+        int[][] dp = new int[n + 1][s1 + 1];
+
+        // Base case: There is 1 way to get sum 0 (by picking nothing)
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            int currentNum = nums[i - 1];
+
+            for (int j = 0; j <= s1; j++) {
+
+                // Option 1: Don't pick the current number
+                dp[i][j] = dp[i - 1][j];
+
+                // Option 2: Pick the current number (if it doesn't exceed current sum j)
+                if (j >= currentNum) {
+                    dp[i][j] += dp[i - 1][j - currentNum];
+                }
+
+            }
+        }
+
+        return dp[n][s1];
     }
 }
