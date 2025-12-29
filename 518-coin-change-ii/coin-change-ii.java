28d49b738d6a8194b40984fde83bb7a0a4772cd5
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2025-12-29 17:43:29 +0530

    Time: 11 ms (62.64%) | Memory: 57.3 MB (21.01%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,29 +1,49 @@
 class Solution {
 
    // memoization 
-    public int solve(int[] coins, int target, int i, int[][] memo) {
+    // public int solve(int[] coins, int target, int i, int[][] memo) {
 
-        if (target == 0) return 1; //we add only when target is 0 
-        if (i < 0 || target < 0) return 0;       //otherwise 0
+    //     if (target == 0) return 1; //we add only when target is 0 
+    //     if (i < 0 || target < 0) return 0;       //otherwise 0
 
-        if(memo[i][target]!= -1)  return memo[i][target];
+    //     if(memo[i][target]!= -1)  return memo[i][target];
 
 
-        int notPick = solve(coins, target, i - 1, memo);
-        int pick = solve(coins, target - coins[i], i, memo);
+    //     int notPick = solve(coins, target, i - 1, memo);
+    //     int pick = solve(coins, target - coins[i], i, memo);
 
-        return memo[i][target]=pick + notPick;
-    }
+    //     return memo[i][target]=pick + notPick;
+    // }
 
     public int change(int amount, int[] coins) {
 
         int target = amount;
         int n=coins.length;
 
-        int[][] memo = new int[n][amount+1];
-
-        for(int[] rows:memo)  Arrays.fill(rows, -1);
+        int[][] dp = new int[n][amount+1];
     
-        return solve(coins, amount, n-1, memo);
+        //base case 
+        for (int i = 0; i < n; i++) dp[i][0] = 1;
+
+        // First coin row
+        for (int j = 0; j <= amount; j++) {
+            if (j % coins[0] == 0)
+                dp[0][j] = 1;
+        }
+
+        for(int i=1; i<n; i++){
+
+            for(int j=0; j<=target; j++){
+
+                int notPick = dp[i-1][j];
+
+                int pick =(j >= coins[i])? dp[i][j-coins[i]]: 0; 
+                
+                dp[i][j]=pick + notPick;
+
+            }
+        }
+
+        return dp[n-1][target];
     }
 }
