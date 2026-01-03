4f107e4896bb3be02ce36d4ac1927bb18beaf516
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-01-03 20:55:12 +0530

    Time: 3 ms (93.85%) | Memory: 42.2 MB (50.26%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public int numOfWays(int n) {
+
+        int MOD = (int)1e9 +7;
+        
+        //base 
+        long aba = 6, abc =6;
+
+        for(int i=2; i<=n;i++){
+
+            long new_aba = ( 3*aba + 2*abc) % MOD;
+            long new_abc = ( 2*aba + 2*abc )% MOD;
+
+            aba = new_aba;
+            abc = new_abc;
+        }
+
+        return (int)((aba+abc)%MOD);
+
+
+        
+    }
+}
