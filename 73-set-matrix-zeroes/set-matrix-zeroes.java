73fc3c011356196272b13b607d8c3e8c4e5a48e2
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-01-03 20:35:32 +0530

    Time: 2 ms (13.91%) | Memory: 47.7 MB (5.58%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,38 +1,39 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean col0 = false;
 
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) col0 = true;
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
+        //create two hashmap
+        HashSet<Integer> rows = new HashSet<Integer> ();
+        HashSet<Integer> cols = new HashSet<Integer> ();
+
+        //add row and col index to hashmap
+
+        for(int i = 0 ; i < matrix.length ; i++) {
+
+            for(int j = 0 ; j < matrix[i].length ; j++) {
+
+                if(matrix[i][j] != 0) continue;
+
+                    cols.add(j);
+                    rows.add(i);
+                
             }
         }
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
+        //for every rows in set make all elements are 0
+        for(Integer i: rows) {
 
-        if (matrix[0][0] == 0) {
-            for (int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
+            for(int j = matrix[0].length - 1 ; j >= 0 ; j--) {
+                
+                matrix[i][j] = 0;
             }
-        }
+        }   
+        //for every col in set make all rows are 0
+        for(Integer j: cols) {
 
-        if (col0) {
-            for (int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
+            for(int i = matrix.length - 1 ; i >= 0 ; i-- ) {
+                
+                matrix[i][j] = 0;
             }
         }
-      
     }
 }
