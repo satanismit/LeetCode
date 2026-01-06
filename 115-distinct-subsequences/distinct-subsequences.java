8dc19d6de140c0c5f19c00e224a59d1bda2aa33a
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-01-06 16:24:42 +0530

    Time: 21 ms (37.02%) | Memory: 54.4 MB (40.58%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,36 +1,28 @@
 class Solution {
 
-    //count no of ways recursion +memoization  
-    public int solve(String s, String t,int i,int j, int[][] memo){
-
-        //base case either 0 or 1
-        if(j<0) return 1;
-        if(i<0) return 0;
-
-        if(memo[i][j]!=-1)  return memo[i][j];
-
-        //explore all possibilities 
-        if(s.charAt(i)==t.charAt(j)) {
-
-            return memo[i][j]=solve(s,t,i-1,j-1, memo)+solve(s,t,i-1,j, memo);
-        }
-
-
-        return memo[i][j]=solve(s, t, i-1,j, memo);
-
-        //return max 
-
-
-    }
+    //tabulation method 
     public int numDistinct(String s, String t) {
 
         int n=s.length(), m= t.length();
 
-        int[][] memo = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
 
-        for(int[] rows:memo) Arrays.fill(rows, -1);
+        //start from base case
+        for(int i=0; i<n; i++)   dp[i][0]=1;
 
-        return solve(s,t,n-1,m-1, memo);
+        for(int i=1; i<=n; i++){
+
+            for(int j=1; j<=m; j++){
+
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]= dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
         
     }
 }
