e3de9252267951bf37a872555b7754392744d4a6
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-01-19 21:17:10 +0530

    Time: 17 ms (44.84%) | Memory: 69.4 MB (41.13%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,47 +1,31 @@
 class Solution {
 
-   //memoization
-    public int solve(int[] prices, int ind, int buy, int n, int[][] memo, int fee){
-
-    //base case 
-    if(ind== n)  return 0;
-
-     int profit = 0;
-
-     if(memo[ind][buy] != -1)  return memo[ind][buy];
-
-    if(buy==1){
-        //take or not take 
-        // buy or not buy
-
-        int take = -prices[ind] + solve(prices, ind+1, 0,n, memo, fee);
-        int notbuy = solve(prices, ind+1, 1,n, memo, fee);
-        profit = Math.max(profit, Math.max(take, notbuy));
-
-    }else{
-
-        //sell or not sell
-        int sell = prices[ind] -fee + solve(prices, ind+1, 1,n, memo, fee); // -fee whenever one transaction is done 
-        int notsell = solve(prices, ind+1, 0,n, memo, fee);
-        profit = Math.max(profit, Math.max(sell, notsell));
-
-    }
-
-    return memo[ind][buy]=profit;
-
-    }
 
     public int maxProfit(int[] prices, int fee) {
-
-    
-    int n= prices.length;
-    int buy= 1;
-
-    int[][] memo = new int[n][2];
-
-    for(int[] rows:memo) Arrays.fill(rows, -1);
-
-    return solve(prices, 0, buy,n, memo, fee);
         
+        int n= prices.length;
+
+        int[][] dp = new int[n+1][2];
+
+        for(int ind=n-1; ind>=0; ind--){
+
+            for(int buy = 0; buy<=1; buy++){
+
+                int profit = 0;
+
+                if(buy==1){
+
+                    profit = Math.max( -prices[ind]+ dp[ind+1][0], dp[ind+1][1]  );
+
+                }else{
+                    profit = Math.max( prices[ind]-fee+dp[ind+1][1],  dp[ind+1][0]);
+
+                }
+
+                dp[ind][buy]= profit;
+            }
+        }
+
+        return dp[0][1];
     }
 }
