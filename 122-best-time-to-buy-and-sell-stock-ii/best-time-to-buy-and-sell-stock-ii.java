276acb0a22ca8f9cc7acaeddc40d003cf6a1e04e
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-01-19 18:45:29 +0530

    Time: 1 ms (18.34%) | Memory: 46.3 MB (15.91%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,33 +1,37 @@
 class Solution {
 
-   
-    public int solve(int[] prices, int ind, int buy, int n, int[][] memo){
+   //tabulation 
+    public int solve(int[] prices, int n, int[][] dp){
 
-    //base case 
-    if(ind== n)  return 0;
+    for(int ind = n-1; ind>=0; ind--){
 
-     int profit = 0;
+        for(int buy=0; buy<=1; buy++){
 
-     if(memo[ind][buy] != -1)  return memo[ind][buy];
+            int profit = 0;
 
-    if(buy==1){
-        //take or not take 
-        // buy or not buy
+            if(buy==1){
+                //take or not take 
+                // buy or not buy
 
-        int take = -prices[ind] + solve(prices, ind+1, 0,n, memo);
-        int notbuy = solve(prices, ind+1, 1,n, memo);
-        profit = Math.max(profit, Math.max(take, notbuy));
+                int take =  -prices[ind] + dp[ind+1][0];
+                int notbuy = dp[ind+1][1];
+                profit = Math.max(profit, Math.max(take, notbuy));
 
-    }else{
+            }else{
 
-        //sell or not sell
-        int sell = prices[ind] + solve(prices, ind+1, 1,n, memo);
-        int notsell = solve(prices, ind+1, 0,n, memo);
-        profit = Math.max(profit, Math.max(sell, notsell));
+                //sell or not sell
+                int sell = prices[ind] + dp[ind+1][1];
+                int notsell = dp[ind+1][0];
+                profit = Math.max(profit, Math.max(sell, notsell));
 
+            }
+
+            dp[ind][buy] = profit;
+        }
     }
+     
 
-    return memo[ind][buy]=profit;
+    return dp[0][1];
 
     }
 
@@ -37,11 +41,12 @@ class Solution {
     int n= prices.length;
     int buy= 1;
 
-    int[][] memo = new int[n][2];
+    int[][] dp = new int[n+1][2];
 
-    for(int[] rows:memo) Arrays.fill(rows, -1);
+    //base 
+    dp[n][0]= dp[n][1] =0;
 
-    return solve(prices, 0, buy,n, memo);
+    return solve(prices,n, dp);
         
     }
 }
