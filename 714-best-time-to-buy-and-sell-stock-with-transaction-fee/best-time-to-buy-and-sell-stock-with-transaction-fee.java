99c20015ed06d7be5307105a0830020a0217c28f
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-01-19 21:11:05 +0530

    Time: 37 ms (7.48%) | Memory: 77.7 MB (13.37%) - LeetSync

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,47 @@
+class Solution {
+
+   
+    public int solve(int[] prices, int ind, int buy, int n, int[][] memo, int fee){
+
+    //base case 
+    if(ind== n)  return 0;
+
+     int profit = 0;
+
+     if(memo[ind][buy] != -1)  return memo[ind][buy];
+
+    if(buy==1){
+        //take or not take 
+        // buy or not buy
+
+        int take = -prices[ind] + solve(prices, ind+1, 0,n, memo, fee);
+        int notbuy = solve(prices, ind+1, 1,n, memo, fee);
+        profit = Math.max(profit, Math.max(take, notbuy));
+
+    }else{
+
+        //sell or not sell
+        int sell = prices[ind] -fee + solve(prices, ind+1, 1,n, memo, fee); // -fee whenever one transaction is done 
+        int notsell = solve(prices, ind+1, 0,n, memo, fee);
+        profit = Math.max(profit, Math.max(sell, notsell));
+
+    }
+
+    return memo[ind][buy]=profit;
+
+    }
+
+    public int maxProfit(int[] prices, int fee) {
+
+    
+    int n= prices.length;
+    int buy= 1;
+
+    int[][] memo = new int[n][2];
+
+    for(int[] rows:memo) Arrays.fill(rows, -1);
+
+    return solve(prices, 0, buy,n, memo, fee);
+        
+    }
+}
