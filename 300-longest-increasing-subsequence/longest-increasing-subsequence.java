2907d9c6edb7557b62951ffd27a346905df4ffa3
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-01-20 18:25:51 +0530

    Time: 90 ms (22.68%) | Memory: 237.6 MB (6.53%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,35 +1,35 @@
 class Solution {
 
-    //memoization
-    public int solve(int ind, int prevind, int[] nums, int[][] memo){
-
-        //base case
-        if(ind==nums.length)  return 0;
-
-        if(memo[ind][prevind+1]!=-1)  return memo[ind][prevind+1];
-        //take
-        int take = 0;     
-    
-        if(prevind==-1 || nums[prevind]<nums[ind]){
-             take =  1+ solve(ind+1, ind, nums, memo);
-        }
-       
-        //not take
-        int nottake = solve(ind+1, prevind, nums, memo);
-
-        return memo[ind][prevind+1]= Math.max(take, nottake);
-
-    }
-
+    //tabulation 
     public int lengthOfLIS(int[] nums) {
 
         int n= nums.length;
 
-        int[][] memo = new int[n][n+1];
+        int[][] dp = new int[n+1][n+1];
 
-        for(int[] rows:memo) Arrays.fill(rows, -1);
+        //start from base case 
 
-        return solve(0,-1, nums, memo);
+        for(int ind= n-1; ind>=0; ind--){
+
+            for(int prevind=ind-1; prevind>=-1; prevind--){
+
+                int take = 0;
+
+                if(prevind==-1 || nums[prevind]<nums[ind]){
+                    take =1+ dp[ind+1][ind+1];
+                }
+
+                int nottake = dp[ind+1][prevind+1];
+
+                dp[ind][prevind+1] = Math.max(take, nottake);
+            }
+        }
+
+        return dp[0][0];
+
+        
+
+        
 
 
         
