d2a8767504986179fffcd5b6dded9e0e11d4d59b
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-01-20 23:05:04 +0530

    Time: 1 ms (73.41%) | Memory: 43 MB (17.70%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.java
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -1,13 +1,11 @@
 class Solution {
 
-    
+    //tabulation 
     public int combinationSum4(int[] nums, int target) {
 
         int n= nums.length;
 
         int[] dp = new int[target+1];
-
-
         dp[0]=1;
 
         for(int i=0; i<=target; i++){
