8ac020087c224d1dce2d6fcbd7857a5a47918b1c
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-01-20 23:04:51 +0530

    Time: 1 ms (73.41%) | Memory: 42.6 MB (82.08%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.java
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -1,40 +1,26 @@
 class Solution {
 
-    public int solve(int[] nums, int target, int[] memo){
-
-        if(memo[target]!=-1)  return memo[target];
-
-        //base case
-        if(target==0)  return 1;
-        if( target<0)  return 0;
-
-        int res =0;
-
-        for(int num:nums){
-
-            if(target>=num){
-                res += solve(nums, target-num, memo);
-            }
-
-        }
-        memo[target] = res;
-
-        return res;
-
-    }
-
-
+    
     public int combinationSum4(int[] nums, int target) {
 
         int n= nums.length;
 
-        int[] memo = new int[target+1];
+        int[] dp = new int[target+1];
 
-        Arrays.fill(memo, -1);
 
-        memo[0]=1;
+        dp[0]=1;
 
-        return solve(nums, target, memo);
+        for(int i=0; i<=target; i++){
+
+            for(int j=0; j<nums.length; j++){
+
+                if(i >= nums[j]){
+                    dp[i] += dp[i-nums[j]];
+                }
+            }
+        }
+
+        return dp[target];
         
     }
 }
