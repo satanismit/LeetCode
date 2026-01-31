dfcd7050d4ed96c3e3b30efe3b66ea1e651618b7
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-01-31 11:46:51 +0530

    Time: 40 ms (94.38%) | Memory: 44.8 MB (16.01%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,27 +1,6 @@
 class Solution {
 
-    // memoization TC = O( n^3 ) ,sc = O(n^2) + O(N) AS
-    int[][] memo;
-
-    public int solve(int[] nums, int i, int j){
-
-        if(i>j)  return 0;
-
-        if(memo[i][j]!=-1)  return memo[i][j];
-
-        int max = Integer.MIN_VALUE;
-
-        for(int ind=i; ind<=j; ind++){
-
-            int cost = (nums[i-1]* nums[ind] * nums[j+1] )+
-                       solve(nums, i,ind-1)+solve(nums, ind+1,j);
-
-            max = Math.max(max, cost);
-        }
-
-        return memo[i][j]= max;
-
-    }
+    // Tabulation  TC = O( n^3 ) ,sc = O(n^2) 
     
     public int maxCoins(int[] nums) {
 
@@ -33,10 +12,33 @@ class Solution {
         arr[0]=1;
         arr[n+1]=1;
 
-        memo = new int[n+2][n+2];
+       int[][] dp = new int[n+2][n+2];
 
-        for(int[] rows:memo) Arrays.fill(rows, -1);
+       //base case alrady satisfied
 
-        return solve(arr,1,n);
+       //reverse changing params
+
+       for(int i=n; i>=1; i--){
+
+        for(int j=1; j<=n; j++){
+
+            if(i>j)  continue;
+
+            int max = Integer.MIN_VALUE;
+
+            for(int ind=i; ind<=j; ind++){
+
+                int cost = (arr[i-1]* arr[ind] * arr[j+1] )+
+                           dp[i][ind-1] + dp[ind+1][j];
+
+                max = Math.max(max, cost);
+            }
+
+            dp[i][j]= max;
+
+        }
+       }
+
+        return dp[1][n];
     }
 }
