558406a59cf6efb4131c30fbaa7ce77462689438
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-02-07 23:03:38 +0530

    Time: 15 ms (10.00%) | Memory: 45.1 MB (5.38%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,87 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    Map<Integer, List<Integer>> adj = new HashMap<>();
+
+    private void createGraph(TreeNode node){
+
+        if(node==null)  return;
+
+
+        if(node.left!=null){
+
+            adj.computeIfAbsent( node.left.val , k-> new ArrayList<>()).add(node.val);
+            adj.computeIfAbsent(node.val, k-> new ArrayList<>()).add(node.left.val);
+        }
+
+        if(node.right!=null){
+             adj.computeIfAbsent( node.right.val , k-> new ArrayList<>()).add(node.val);
+            adj.computeIfAbsent(node.val, k-> new ArrayList<>()).add(node.right.val);
+
+        }
+
+        createGraph(node.left);
+        createGraph(node.right);
+
+    }
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        if(k==0) {
+            ans.add(target.val);
+            return ans;
+        }
+
+        createGraph(root);
+
+
+        Queue<Integer> q= new LinkedList<>();
+        Set<Integer> set = new HashSet<>();
+
+
+        q.offer(target.val);
+
+        while(!q.isEmpty()){
+
+            k--;
+            int size = q.size();
+
+            for(int i=0; i<size; i++){
+                
+                int node = q.poll();
+                set.add(node);
+                if(adj.containsKey(node)){
+
+                    for(int it:adj.get(node)){
+
+                        if(!set.contains(it)){
+                            q.offer(it);
+                        }
+                    }
+
+                }
+                
+            }
+
+            if(k==0){
+                while(!q.isEmpty()){
+                    ans.add(q.poll());
+                }
+            }
+            
+        }
+
+        return ans;
+        
+    }
+}
