9ecd78bdac534c1e3fd125ca747ef3abd8768248
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-02-08 13:09:41 +0530

    Time: 4 ms (20.46%) | Memory: 49.1 MB (24.75%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,57 +1,55 @@
 class Solution {
 
-    private static void dfs(ArrayList<ArrayList<Integer>> adj_list, int i, boolean[] vis){
-        
-        if(vis[i]!=true){
-            
-            vis[i]=true; 
-            //call for its neighbors
-            for(int ele:adj_list.get(i)){
-                dfs(adj_list, ele, vis);
+    public void dfs(List<List<Integer>> adj, int node,boolean[] vis){
+
+        if(!vis[node]){
+
+            vis[node]=true;
+
+            for(int it:adj.get(node)){
+
+                dfs(adj, it, vis);
             }
         }
     }
 
     public int findCircleNum(int[][] isConnected) {
+        
+        List<List<Integer>> adj = new ArrayList<>();
 
-        int V = isConnected.length;
-        boolean[] vis = new boolean[V+1];
-        
-        //we have an adjancy matrix need to conver into adjancy list 
-        ArrayList<ArrayList<Integer>> adj_list = new ArrayList<>();
-        
-        //add arraylist
-        for(int i=0; i<V; i++) adj_list.add(new ArrayList<>());
-        
-        
-        for(int i=0; i<V; i++){
-            
-            for(int j=0; j<V; j++){
-            
-                //if matrix have edge we add it 
-                if(isConnected[i][j]==1 && i!=j){
-                    
-                    adj_list.get(i).add(j);
-                   adj_list.get(j).add(i);
+        int m= isConnected.length;
+        int n= isConnected[0].length;
+
+        for(int i=0; i<m; i++)  adj.add(new ArrayList<>());
+
+
+        for(int i=0; i<m; i++){
+
+            for(int j=0; j<n; j++){
+
+                if(isConnected[i][j]==1){
+
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
                 }
-                
-                
-            }
-            
-        }
-        
-        int cnt = 0;
-        
-        //call for all components in graph and track cnt 
-        for(int i=0; i<V; i++){
-            
-            if(vis[i]!=true){
-                cnt++;
-                dfs(adj_list, i, vis);
             }
         }
-        
-        return cnt;
-        
+
+        // now we have adjlist 
+        boolean[] vis  = new boolean[m];
+
+        int count=0;
+
+        for(int i=0; i<m; i++){
+
+            if(!vis[i]){
+                count++;
+                dfs(adj, i, vis);
+            }
+        }
+
+        return count;
+
+
     }
 }
