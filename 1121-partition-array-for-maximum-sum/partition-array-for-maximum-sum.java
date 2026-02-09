daf0856d405477469ebfb6c47687b20bd54e4d28
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-02-09 18:37:22 +0530

    Time: 7 ms (54.09%) | Memory: 44.2 MB (12.87%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,12 +1,14 @@
 class Solution {
 
-    int[] memo;
+    // tabulation 
 
-    public int solve(int[] arr, int k, int ind){
+    public int maxSumAfterPartitioning(int[] arr, int k) {
 
-        if(ind==arr.length) return 0;
+        int n= arr.length;
 
-        if(memo[ind]!=-1)  return memo[ind];
+        int[] dp = new int[n+1];
+
+        for(int ind=n-1; ind>=0; ind--){
 
         int ans=Integer.MIN_VALUE;
         int length=0, currmax= Integer.MIN_VALUE;
@@ -17,23 +19,16 @@ class Solution {
 
             currmax = Math.max(currmax, arr[j]);
             
-            sum = currmax*length + solve(arr, k, j+1);
+            sum = currmax*length + dp[j+1];
 
             ans = Math.max(ans, sum);
             
         }
 
-        return memo[ind]=ans;
+        dp[ind]=ans;
 
-    }
+        }
 
-    public int maxSumAfterPartitioning(int[] arr, int k) {
-
-        int n= arr.length;
-
-        memo = new int[n];
-        Arrays.fill(memo, -1);
-
-        return solve(arr, k, 0);
+        return dp[0];
     }
 }
