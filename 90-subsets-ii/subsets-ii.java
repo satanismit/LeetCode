3b3b7bfc47e96425c0c3373490dc0e16ce3f8bee
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-04-09 22:00:48 +0530

    Time: 3 ms (56.19%) | Memory: 45 MB (66.25%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,41 +1,31 @@
 class Solution {
 
-    List<List<Integer>> ans;
+    public void solve(List<List<Integer>> ans,int[] nums , int ind, List<Integer> ds){
 
-    public void solve(List<Integer> list,int[] nums,int i){
-
-        //base 
-        if(i==nums.length){
-            ans.add(new ArrayList<>(list));
-            return;
-        }
-
-        list.add(nums[i]);
-        solve(list, nums, i+1);
-
-        //backtrack 
-        list.remove(list.size()-1);
-
-        //remove duplicate 
-        while(i+1<nums.length && nums[i]==nums[i+1])
-            i++;
-
-        solve(list, nums, i+1);
+      ans.add(new ArrayList<>(ds));
+      
+      for(int i=ind; i<nums.length; i++){
+        
+        if(i!=ind && nums[i]==nums[i-1]) continue;
+        
+          ds.add(nums[i]);
+          solve(ans, nums, i+1, ds);
+          ds.remove(ds.size()-1);
+      }
 
 
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
 
-        Arrays.sort(nums);
-        
-        ans = new ArrayList<>();
         int n= nums.length;
 
+        Arrays.sort(nums);
 
-        solve(new ArrayList<>(), nums,0);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        solve(ans,nums, 0, new ArrayList<>());
 
         return ans;
-        
     }
 }
