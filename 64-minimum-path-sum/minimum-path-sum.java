539c91fe0c92872e5aa4267ae4f68ca2687bafa9
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-04-26 10:45:56 +0530

    Time: 6 ms (9.74%) | Memory: 50.2 MB (72.75%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,30 +1,33 @@
 class Solution {
 
-    int[][] memo;
-
-    public int solve(int[][] grid, int i, int j){
-
+    //tabulation method 
     
-        if(i==0 && j==0)  return grid[i][j];
-        if(i<0 || j<0)  return (int)1e9;
-
-        if(memo[i][j]!=-1) return memo[i][j];
-
-        int up = grid[i][j] + solve( grid, i-1, j);
-        int left = grid[i][j] + solve(grid, i, j-1);
-
-        return memo[i][j]=Math.min(up, left);
-
-    }
-
     public int minPathSum(int[][] grid) {
 
         int n= grid.length;
-        memo = new int[n][grid[0].length];
+        int m=grid[0].length;
 
-        for(int[] rows: memo)  Arrays.fill(rows, -1);
+        int[][] dp = new int[n][m];
 
-        return solve(grid, n-1, grid[0].length-1);
+        for(int i=0; i<n; i++){
+            
+            for(int j=0; j<m; j++){
+
+                if(i==0 && j==0) dp[i][j]=grid[i][j];
+                else{
+
+                    int up=Integer.MAX_VALUE;
+                    int left = Integer.MAX_VALUE;
+
+                    if(i>0)  up = grid[i][j] + dp[i-1][j];
+                    if(j>0)  left = grid[i][j] + dp[i][j-1];
+
+                    dp[i][j]=Math.min(up, left);
+                }
+            }
+        }
+
+        return dp[n-1][m-1];
         
     }
 }
