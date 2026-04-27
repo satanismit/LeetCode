05c26dc39dd8327429e9d50764f31fcf06ee1e42
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-04-27 17:38:19 +0530

    Time: 2 ms (99.88%) | Memory: 52.3 MB (78.31%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,34 +1,51 @@
 class Solution {
 
+    //boyre moore's voting algorithm - TC: O(n) , sc: O(n)
+    
     public List<Integer> majorityElement(int[] nums) {
 
-         int n = nums.length;
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        
+        int n=nums.length;
+
+        int cand1 =0, cand2=0;
+        int c1 =0, c2=0;
+
+        for(int num:nums){
+
+            if(num==cand1){
+
+                c1++;
+            }else if( num==cand2){
+
+                c2++;
+            }else if( c1==0){
+                cand1= num;
+                c1++;
+            }else if (c2==0){
+                cand2=num;
+                c2++;
+            }else{
+                c1--;
+                c2--;
+            }
+
+        }
+
+        c1=0;
+        c2=0;
+
+        //verify and add 
         List<Integer> res = new ArrayList<>();
 
-        // find frequency of each number
-        for (int ele : nums)
-            freq.put(ele, freq.getOrDefault(ele, 0) + 1);
+        for(int i=0; i<n; i++){
 
-   
-        for (Map.Entry<Integer, Integer> it : freq.entrySet()) {
-            int ele = it.getKey();
-            int cnt = it.getValue();
-
-            if (cnt > n / 3)
-                res.add(ele);
+            if(nums[i]==cand1) c1++;
+            else if(nums[i]==cand2) c2++;
         }
 
-        if (res.size() == 2 && res.get(0) > res.get(1)) {
-            int temp = res.get(0);
-            res.set(0, res.get(1));
-            res.set(1, temp);
-        }
+        if( c1 > n/3) res.add(cand1);
+        if(c2> n/3) res.add(cand2);
 
         return res;
-
-        
         
     }
 }
