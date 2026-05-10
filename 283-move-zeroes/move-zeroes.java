34ea30ba73bcea28f689fef411c7b47afdd0d966
Satani Smit Hiteshbhai <161738842+satanismit@users.noreply.github.com>
2026-05-10 21:17:21 +0530

    Time: 1 ms (99.84%) | Memory: 47.9 MB (19.47%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,18 +1,22 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int n=nums.length;
-        int left=0;
-        
-        for(int right=0; right<n; right++){
 
-                //found non zero element then swap 
-            if(nums[right]!=0){
-                int temp=nums[right];
-                nums[right]=nums[left];
-                nums[left]=temp;
-                left++;
+    public void moveZeroes(int[] nums) {
+
+        int n= nums.length;
+
+        int j=0;
+
+        for(int i=0; i<n; i++){
+
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                j++;
             }
-            
+
+        }
+
+        while(j<n){
+            nums[j++]=0;
         }
         
     }
